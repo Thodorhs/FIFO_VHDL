cb41f20a20e60ccf3bad1966c5b17153b0a925ea
Thodorhs <thodp@ics.forth.gr>
2025-03-07 20:48:00 +0200

    [fix] Fix entity

--- async/async_fifo.vhd
+++ async/async_fifo.vhd
@@ -2,35 +2,33 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity async_fifo is 
-	generic (
-		f_WIDTH : natural := 10;
-		f_DEPTH : natural := 8;
-	);
+entity async_fifo is
+ generic (
+	f_DATA_WIDTH : natural := 10;
+	f_ADDRESS_WIDTH : natural := 5;
+ );
 	
-	port (
-		f_wclk : in std_logic;
-		f_rclk : in std_logic;
-		f_reset : in std_logic;
-		
-		-- OCCUPANCY
-		f_occupancy_in : out std_logic;
-		f_occupancy_out : out std_logic;
-		
-		-- WRITE
-		f_write_en : in std_logic;
-		f_write_data : in std_logic_vector( f_WIDTH-1 downto 0);
-		f_full : out std_logic;
-		
-		-- READ
-		f_read_en : in std_logic;
-		f_read_data : in std_logic_vector( f_WIDTH-1 downto 0);
-		f_empty : out std_logic;
-	);
-	
+ port (
+	reset : in std_logic;
+	wclk : in std_logic;
+	rclk : in std_logic;
+
+	-- OCCUPANCY
+	fifo_occu_in : out std_logic_vector(4 downto 0);
+	fifo_occu_out : out std_logic_vector(4 downto 0);
+
+	-- WRITE
+	write_enable : in std_logic;
+	write_data_in : in std_logic_vector(7 downto 0);
+
+	-- READ
+	read_enable : in std_logic;
+	read_data_out : out std_logic_vector(7 downto 0)
+ );
 end async_fifo;
 
 
 architecture ar of async_fifo is
+
 
 end ar;
